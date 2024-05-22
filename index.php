83500b54d646af0b24e03ff59b8d2bf1d90f8b0b
SergioRamirezNavarro <ramireznavarro.sergio@gmail.com>
2024-05-22 19:48:02 +0200

    bootstrapped

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
             text-align: left;
         }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -118,6 +119,6 @@
     <hr>
     <button onclick="window.location.href='add_author.php'">Agregar Autor</button>
     <button onclick="window.location.href='add_book.php'">Agregar Libro</button>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
